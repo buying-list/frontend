1e0efed5fb1746acbae4df6b5c266742a5c793a4
Jonatan Flores <ti02@alfasistemas.inf.br>
2020-03-31 23:18:04 -0300

    mockup application functionality

--- src/pages/ItemList/index.js
+++ src/pages/ItemList/index.js
@@ -1,16 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { v4 as uuidv4 } from "uuid";
 
 import AppNavbar from "../../components/AppNavbar";
 
 import "./styles.css";
 
 export default function ItemList() {
+  const [items, setItems] = useState([
+    { id: uuidv4(), name: "Eggs" },
+    { id: uuidv4(), name: "Milk" },
+    { id: uuidv4(), name: "Steak" },
+    { id: uuidv4(), name: "Water" }
+  ]);
+
   return (
     <>
       <AppNavbar />
-      <div id="item-list">
-        <h1>Item List</h1>
-      </div>
+      <Container>
+        <Button
+          color="dark"
+          style={{ marginBottom: "2rem" }}
+          onClick={() => {
+            const name = prompt("Enter Item");
+            if (name) {
+              setItems([...items, { id: uuidv4(), name }]);
+            }
+          }}
+        >
+          Add Item
+        </Button>
+
+        <ListGroup>
+          <TransitionGroup className="shopping-list">
+            {items.map(({ id, name }) => (
+              <CSSTransition key={id} timeout={500} classNames="fade">
+                <ListGroupItem>
+                  <Button
+                    className="remove-btn"
+                    color="danger"
+                    size="sm"
+                    onClick={() => {
+                      setItems(items.filter(item => item.id !== id));
+                    }}
+                  >
+                    &times;
+                  </Button>
+                  {name}
+                </ListGroupItem>
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </ListGroup>
+      </Container>
     </>
   );
 }
